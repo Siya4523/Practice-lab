fd4f68fb4a0e12b53f376899c815c00a75629c32
Siya4523 <rathodkaveri1289@gmail.com>
2025-01-06 17:19:16 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,9 @@
+# Declare two variables
+num1 = 10
+num2 = 20
+
+# Using ternary operator to find the largest number
+largest = num1 if num1 > num2 else num2
+
+# Print the largest number
+print(f"The largest number is: {largest}")
